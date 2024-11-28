f2159643cd79865a702e8fbc831ad754df962308
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-28 15:09:04 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+	return (
+		<footer>
+			<div>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+				cupiditate quos nobis provident fugiat error facilis. Fuga, ullam ipsam.
+				Quia adipisci consequuntur nihil obcaecati ad sit fugit iure quos quam.
+			</div>
+		</footer>
+	);
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+	return (
+		<main>
+			<div className="card">titolo </div>
+		</main>
+	);
+}
