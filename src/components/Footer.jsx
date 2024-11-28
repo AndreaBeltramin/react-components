449a7224f24c9da2ffc3d535f4869fd6df371b00
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-28 16:36:43 +0100

    modifiche aspetto post

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 export default function Footer() {
 	return (
 		<footer>
-			<div className="container">
+			<div className="container-footer mx-auto">
 				<div className="text-center">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-					Reprehenderit cupiditate quos nobis provident fugiat error facilis.
-					Fuga, ullam ipsam.
-				</div>{" "}
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis,
+					debitis quis reprehenderit ut porro, quos, amet inventore corrupti
+					ducimus tempora deserunt consequuntur laborum voluptates magni eos
+					sunt veritatis possimus laudantium. Lorem ipsum dolor, sit amet
+					consectetur adipisicing elit. Fuga provident inventore laboriosam nam
+					cumque praesentium dolorem atque facere deleniti, blanditiis aliquam
+					laudantium.
+				</div>
 			</div>
 		</footer>
 	);
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,14 +4,14 @@ export default function Main() {
 	return (
 		<main>
 			<div className="container">
-				<div className="card mb-5">
+				<div className="card">
 					<img
 						src={image}
 						className="card-img-top img-fluid mx-auto"
 						alt="montagna"
 					/>
 					<div className="card-body">
-						<h5 className="card-title">Titolo del Post</h5>
+						<h2 className="card-title">Titolo del Post</h2>
 						<p className="card-text">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ad
 							magnam delectus tempore reprehenderit quasi eveniet, sapiente eos
